6bbdb918d85f8cabf6ce4eaa80a117d9cb17e1c5
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-06-12 11:28:17 +0300

    added friend response skeleton

---
+++ vkapi/friends.py
@@ -0,0 +1,24 @@
+import dataclasses
+import typing as tp
+
+
+@dataclasses.dataclass(frozen=True)
+class FriendsResponse:
+    count: int
+    items: tp.Union[tp.List[int], tp.List[tp.Dict[str, tp.Any]]]
+
+
+def get_friends(
+        user_id: int, count: int = 5000, offset: int = 0, fields: tp.Optional[tp.List[str]] = None
+) -> FriendsResponse:
+    """
+    Получить список идентификаторов друзей пользователя или расширенную информацию
+    о друзьях пользователя (при использовании параметра fields).
+
+    :param user_id: Идентификатор пользователя, список друзей для которого нужно получить.
+    :param count: Количество друзей, которое нужно вернуть.
+    :param offset: Смещение, необходимое для выборки определенного подмножества друзей.
+    :param fields: Список полей, которые нужно получить для каждого пользователя.
+    :return: Список идентификаторов друзей пользователя или список пользователей.
+    """
+    pass
