af0150e8da4d67cb764072f23af0fd5c32f96808
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-06-12 10:48:56 +0300

    updated 'session'

--- vkapi/session.py
+++ vkapi/session.py
@@ -1,12 +1,27 @@
 import typing as tp
 
 import requests
+from requests.adapters import HTTPAdapter, Retry
+
+
+class TimeoutHTTPAdapter(HTTPAdapter):
+    def __init__(self, timeout, *args, **kwargs):
+        self.timeout = timeout
+        if "timeout" in kwargs:
+            self.timeout = kwargs["timeout"]
+            del kwargs["timeout"]
+        super().__init__(*args, **kwargs)
+
+    def send(self, request, **kwargs):
+        timeout = kwargs.get("timeout")
+        if timeout is None:
+            kwargs["timeout"] = self.timeout
+        return super().send(request, **kwargs)
 
 
 class Session(requests.Session):
     """
     Сессия.
-
     :param base_url: Базовый адрес, на который будут выполняться запросы.
     :param timeout: Максимальное время ожидания ответа от сервера.
     :param max_retries: Максимальное число повторных запросов.
@@ -20,10 +35,18 @@ class Session(requests.Session):
             max_retries: int = 3,
             backoff_factor: float = 0.3,
     ) -> None:
-        pass
+        super().__init__()
+        self.base_url = base_url
 
-    def get(self, url, **kwargs: tp.Any) -> requests.Response:
-        pass
+        super().mount(self.base_url, TimeoutHTTPAdapter(timeout=timeout,
+                                                        max_retries=Retry(
+                                                            total=max_retries,
+                                                            status_forcelist=[500, 503],
+                                                            backoff_factor=backoff_factor,
+                                                        )))
 
-    def post(self, url, data=None, json=None, **kwargs: tp.Any) -> requests.Response:
-        pass
+    def get(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:
+        return super().get(f'{self.base_url}/{url}', *args, **kwargs)
+
+    def post(self, url: str, *args: tp.Any, **kwargs: tp.Any) -> requests.Response:
+        return super().post(f'{self.base_url}/{url}', *args, **kwargs)
